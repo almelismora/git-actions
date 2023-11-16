8720e4cd2b2d735750b9633f22b7ea8850aa81c4
Elis Mora <moralmedina8@gmail.com>
2023-11-16 19:33:22 +0100

    El resto de funciones y sus pruebas añadidas

--- suma.py
+++ suma.py
@@ -1,8 +1,22 @@
 def sumar(x, y):
     return x + y
 
+def restar(x, y):
+    return  x - y
+
+
+def multiplicar(x, y):
+    return x * y
+
+def dividir(x, y):
+    return x / y
+
+
 
 if __name__ == '__main__':
     print(sumar(5, 3))
+    print(restar(5, 3))
+    print(multiplicar(5, 3))
+    print(dividir(5, 3))
+    print(dividir(5, 0))
 
-    
--- test_suma.py
+++ test_suma.py
@@ -1,5 +1,5 @@
 import unittest
-from suma import sumar
+from suma import sumar, restar, multiplicar, dividir
 
 class TestSumar(unittest.TestCase):
 
@@ -8,6 +8,33 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+class TestRestar(unittest.TestCase):
+
+    def test_restar(self):
+
+        self.assertEqual(restar(10, 5), 5)
+        self.assertEqual(restar(-10, 5), -15)
+        self.assertEqual(restar(-10, -5), -5)
+
+class TestMultiplicar(unittest.TestCase):
+
+    def test_multiplicar(self):
+
+        self.assertEqual(multiplicar(3, 3), 9)
+        self.assertEqual(multiplicar(-3, 3), -9)
+        self.assertEqual(multiplicar(-3, -3), 9)
+
+class TestDividir(unittest.TestCase):
+
+    def test_dividir(self):
+
+        self.assertEqual(dividir(4, 2), 2.0)
+        self.assertEqual(dividir(-4, 2), -2.0)
+        self.assertEqual(dividir(-4, -2), 2.0)
+        
+        with self.assertRaises(ZeroDivisionError):
+           dividir(4, 0)
 
 
 if __name__ == '__main__':
